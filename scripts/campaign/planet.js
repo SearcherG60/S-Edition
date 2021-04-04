f099cd3373270d8f5134779dfa0e18c6eb8e568b
GameSearcher <71985492+GameSearcher@users.noreply.github.com>
2021-04-04 21:23:50 +0300

    Add files via upload

--- scripts/campaign/planet.js
+++ scripts/campaign/planet.js
@@ -1,8 +1,13 @@
 Events.on(ClientLoadEvent, () => {
    
-    const absoluteZero = new SectorPreset("absoluteZero", Planets.serpulo, 2);
+    const absoluteZero = new SectorPreset("absoluteZero", Planets.serpulo, 205);
     absoluteZero.localizedName = "Absolute Zero";
     absoluteZero.difficulty = 2;
     absoluteZero.alwaysUnlocked = true;
+
+    const eruption = new SectorPreset("eruption", Planets.serpulo, 3);
+    eruption.localizedName = "Eruption";
+    eruption.difficulty = 5;
+    eruption.alwaysUnlocked = true;
 
 });
